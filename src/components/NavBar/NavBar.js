4d102e32fdac50ecc795577d9fda1b4028b98ef1
anissval@gmail.com <anahi@anahis-MacBook-Air.local>
2021-06-03 00:17:53 -0300

    adding menu e-commerce to the project.

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import './NavBar.css';
+
+export const NavBar = () => {
+    return <div>
+        <nav className="navigation">
+            <ul className="navbar">
+                <li><a href="#">Baby Outfits</a></li>
+                <li><a href="#">Enteritos</a></li>
+                <li><a href="#">Abrigo</a></li>
+                <li><a href="#">bodies</a></li>
+                <li><a href="#">Sacos de dormir</a></li>
+            </ul>
+        </nav>
+    </div>
+}
